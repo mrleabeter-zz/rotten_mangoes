fed2f29c9f65326f560cfcc58eb063bb56f04877
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-20 19:41:40 -0400

    Added sign up, log in, and log out links, as well as a footer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to movies_path
+      redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
     else
+      flash.now[:alert] = "Log in failed..."
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "Adios!"
+  end
+  
 end
