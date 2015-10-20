bce06c996e361c77ee644638e621c788d63d8431
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-20 16:10:32 -0400

    Fixed all the fucking bullshit environment issues I was having that wasted over 2 hours of my time.  Still Angry

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RottenMangoes::Application.routes.draw do
+  resources :movies
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,3 @@ RottenMangoes::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
